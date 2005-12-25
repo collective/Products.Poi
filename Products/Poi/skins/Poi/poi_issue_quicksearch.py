2c387048fb516a7ea7498c27dc97fa978b707f15
Martin Aspeli <optilude@gmail.com>
2005-12-25 19:48:47 +0000

    Adding issue quicksearch to tracker front page
    svn path=/Poi/trunk/; revision=15256

---
+++ Products/Poi/skins/Poi/poi_issue_quicksearch.py
@@ -0,0 +1,21 @@
+## Python Script "poi_issue_quicksearch"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=searchText
+##title=Parse a quicksearch string and jump to the appropriate issue or search result page
+##
+
+tracker = context
+response = context.REQUEST.RESPONSE
+
+issueId = searchText
+if issueId.startswith('#'):
+    issueId = issueId[1:]
+    
+if tracker.has_key(issueId):
+    response.redirect('%s/%s' % (tracker.absolute_url(), issueId,))
+else:
+    response.redirect('%s/poi_issue_search?SearchableText=%s' % (tracker.absolute_url(), searchText,))
