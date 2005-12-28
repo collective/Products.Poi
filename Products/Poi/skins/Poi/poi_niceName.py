163357cd9e60807e1e291a9fb7161c120359df71
Rocky Burt <rocky@serverzen.com>
2005-12-28 17:14:13 +0000

    Added new poi_niceName() skin script which will take a username and try to get the appropriate full name for that username if it exists.  Otherwise it falls back to simply returning the username.  This script is now used to populate the name of an issue creator on the main issue view and response creators in the response listing.
    svn path=/Poi/trunk/; revision=15356

---
+++ Products/Poi/skins/Poi/poi_niceName.py
@@ -0,0 +1,18 @@
+##title=
+##parameters=username=None,user=None
+
+if not username and not user:
+    raise ValueError('Both username and user cannot be empty')
+
+if username:
+    from Products.CMFCore.utils import getToolByName
+    portal_membership = getToolByName(context, 'portal_membership')
+    user = portal_membership.getMemberById(username)
+else:
+    username = user.getUserName()
+    
+niceName = username
+if user:
+    niceName = user.getProperty('fullname') or username
+
+return niceName
