ca084f2dc6514203433c615a4a3523395d9b4e45
panjunyong <panjunyong@localhost>
2006-03-01 12:17:30 +0000

    quote the search text
    svn path=/Poi/trunk/; revision=19927

--- Products/Poi/skins/Poi/poi_issue_quicksearch.py
+++ Products/Poi/skins/Poi/poi_issue_quicksearch.py
@@ -7,6 +7,7 @@
 ##parameters=searchText
 ##title=Parse a quicksearch string and jump to the appropriate issue or search result page
 ##
+from Products.PythonScripts.standard import url_quote
 
 tracker = context
 response = context.REQUEST.RESPONSE
@@ -18,4 +19,4 @@ if issueId.startswith('#'):
 if tracker.has_key(issueId):
     response.redirect('%s/%s' % (tracker.absolute_url(), issueId,))
 else:
-    response.redirect('%s/poi_issue_search?SearchableText=%s' % (tracker.absolute_url(), searchText,))
+    response.redirect('%s/poi_issue_search?SearchableText=%s' % (tracker.absolute_url(), url_quote(searchText),))
