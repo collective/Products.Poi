2e97c533252ffaa8ed3096f4eba79dd5dec2e671
Martin Aspeli <optilude@gmail.com>
2006-11-05 22:34:38 +0000

    New generate script (svn up to AGX trunk!)
    svn path=/Poi/trunk/; revision=32797

--- Products/Poi/model/generate.sh
+++ Products/Poi/model/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ArchGenXML.py Poi.zuml ../../Poi \
+ArchGenXML.py Poi.zuml ../ \
               --e-mail 'optilude@gmx.net' \
               --copyright 'Copyright (c) 2004 Martin Aspeli' \
               --author 'Martin Aspeli'
