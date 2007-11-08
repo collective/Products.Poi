534a8b8e1c941cbbcd3d34d93562d620b3068d3d
Maurits van Rees <maurits@vanrees.org>
2007-11-08 21:26:10 +0000

    Remove unneeded msgids from poi-manual.pot. Fix rebuildAll.sh script so that it runs without errors. Rebuild poi.pot. Please use i18ndude 3.0 or higher from now on.
    svn path=/Poi/trunk/; revision=53498

--- Products/Poi/i18n/rebuildAll.sh
+++ Products/Poi/i18n/rebuildAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i18ndude rebuild-pot --pot poi.pot --create poi --merge poi-manual.pot `find ../skins/ -iregex '.*\..?pt$'`
+i18ndude rebuild-pot --pot poi.pot --create Poi --merge poi-manual.pot --exclude="poi-my-issues-rss.xml.pt poi-orphaned-issues-rss.xml.pt poi-issue-search-rss.xml.pt" ../
 
 i18ndude sync --pot plone-poi.pot `find . -iregex '.*plone-.*\.po$'`
 
