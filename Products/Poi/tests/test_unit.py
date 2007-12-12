7c1b62e41a28eb4e3cbeba08f8e7e19a9b84818a
Maurits van Rees <maurits@vanrees.org>
2007-12-12 21:47:39 +0000

    Start adding unit tests for link detection.
    svn path=/Poi/trunk/; revision=55410

---
+++ Products/Poi/tests/test_unit.py
@@ -0,0 +1,14 @@
+import unittest
+from zope.testing import doctest
+from zope.testing import doctestunit
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    return unittest.TestSuite([
+        doctestunit.DocFileSuite('linkdetection.txt',
+                                 package='Products.Poi.tests',
+                                 optionflags=OPTIONFLAGS),
+        ])
