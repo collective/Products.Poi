dc58fe5a4da9e393b2fc7aa1f5393c4b6af1de58
Maurits van Rees <maurits@vanrees.org>
2007-12-27 22:15:24 +0000

    Remove UML model as we do not use that anymore.
    svn path=/Poi/trunk/; revision=56202

--- Products/Poi/model/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ArchGenXML.py Poi.zuml ../ \
-              --e-mail 'optilude@gmx.net' \
-              --copyright 'Copyright (c) 2004 Martin Aspeli' \
-              --author 'Martin Aspeli'
-              
-              
