43829e8d2b128ffb2ab104724f29dfb6d3e44e17
Maurits van Rees <maurits@vanrees.org>
2008-02-01 20:33:55 +0000

    Add setup.py and Products dir and add svn:ignores.
    svn path=/Products.Poi/trunk/; revision=58030

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+versionfile = open(os.path.join('Products', 'Poi', 'version.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
+setup(name='Products.Poi',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Martin Aspeli',
+      author_email='plone-users@lists.sourceforge.net',
+      url='http://plone.org/products/poi',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
