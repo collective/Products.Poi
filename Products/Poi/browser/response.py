647b8c6fd5d6de64182592244953182621cd8646
Maurits van Rees <maurits@vanrees.org>
2008-02-22 20:58:59 +0000

    Add unit tests for browser.response.voc2dict.
    svn path=/Products.Poi/branches/maurits-inline-response-adding/; revision=59218

--- Products/Poi/browser/response.py
+++ Products/Poi/browser/response.py
@@ -14,7 +14,20 @@ from plone.memoize.view import memoize
 def voc2dict(vocab, current=None):
     """Make a dictionary from a vocabulary.
 
-    ('a', "The letter A") -> dict(value='a', label="The letter A")
+    >>> from Products.Archetypes.atapi import DisplayList
+    >>> vocab = DisplayList()
+    >>> vocab.add('a', "The letter A")
+    >>> voc2dict(vocab)
+    [{'checked': '', 'value': 'a', 'label': 'The letter A'}]
+    >>> vocab.add('b', "The letter B")
+    >>> voc2dict(vocab)
+    [{'checked': '', 'value': 'a', 'label': 'The letter A'}, {'checked': '', 'value': 'b', 'label': 'The letter B'}]
+    >>> voc2dict(vocab, current='c')
+    [{'checked': '', 'value': 'a', 'label': 'The letter A'}, {'checked': '', 'value': 'b', 'label': 'The letter B'}]
+    >>> voc2dict(vocab, current='b')
+    [{'checked': '', 'value': 'a', 'label': 'The letter A'}, {'checked': 'checked', 'value': 'b', 'label': 'The letter B'}]
+
+
     """
     options = []
     for value, label in vocab.items():
