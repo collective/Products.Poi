ee54d8d5dae8857acf7fbac668d1bb131f213acd
Maurits van Rees <maurits@vanrees.org>
2008-03-01 12:43:00 +0000

    Add first tests for migration of individual issue.
    svn path=/Products.Poi/branches/maurits-inline-response-adding/; revision=59785

---
+++ Products/Poi/tests/test_migration.py
@@ -0,0 +1,17 @@
+from zope.testing import doctest
+import unittest
+from Testing.ZopeTestCase import ZopeDocFileSuite as Suite
+from Products.Poi.tests.ptc import PoiTestCase
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    suites = [
+        Suite('migration.txt',
+              package='Products.Poi.tests',
+              optionflags=OPTIONFLAGS,
+              test_class=PoiTestCase)
+        ]
+    return unittest.TestSuite(suites)
