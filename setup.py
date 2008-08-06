9aed474972e34b0bed8bf6bc73db0a8dde80db84
Maurits van Rees <maurits@vanrees.org>
2008-08-06 21:32:58 +0000

    Added branch 1.1 for Products.Poi. Will contain collective/Poi as svn:external. Meant for creating eggs. Let's see if this works nicely. :-)
    svn path=/Products.Poi/branches/1.1/; revision=69598

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+versionfile = open(os.path.join('Products', 'Poi', 'version.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
+readmefile = open(os.path.join('Products', 'Poi', 'README.txt'))
+readme = readmefile.read().strip()
+readmefile.close()
+
+historyfile = open(os.path.join('Products', 'Poi', 'HISTORY.txt'))
+history = historyfile.read().strip()
+historyfile.close()
+
+long_description = readme + "\n\n" + history
+
+setup(name='Products.Poi',
+      version=version,
+      description="Poi: A friendly issue tracker",
+      long_description=long_description,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Martin Aspeli',
+      author_email='plone-users@lists.sourceforge.net',
+      url='http://plone.org/products/poi',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.AddRemoveWidget',
+          'Products.DataGridField',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
