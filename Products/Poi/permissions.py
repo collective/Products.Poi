b1298910cba1b567b7497dfb36eb7455d3c49974
Maurits van Rees <maurits@vanrees.org>
2009-12-29 22:36:55 +0000

    Define our permissions in zcml and configure them in rolemap.xml.
    svn path=/Products.Poi/trunk/; revision=107681

--- Products/Poi/permissions.py
+++ Products/Poi/permissions.py
@@ -15,13 +15,3 @@ ModifyIssueTags           = "Poi: Modify issue tags"
 ModifyIssueWatchers       = "Poi: Modify issue watchers"
 ModifyIssueTargetRelease  = "Poi: Modify issue target release"
 UploadAttachment          = "Poi: Upload attachment"
-
-# These can probably all be done in rolemap.xml.
-CMFCorePermissions.setDefaultRoles(EditResponse, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueSeverity, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueAssignment, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueState, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueTags, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueWatchers, ['Manager'])
-CMFCorePermissions.setDefaultRoles(ModifyIssueTargetRelease, ['Manager'])
-CMFCorePermissions.setDefaultRoles(UploadAttachment, ['Manager', 'Member'])
