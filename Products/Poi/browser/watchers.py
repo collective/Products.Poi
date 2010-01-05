3b573217e1c8afe6034f58417b7cc34a7d9c78d7
Maurits van Rees <maurits@vanrees.org>
2010-01-05 00:45:42 +0000

    try toggle watching new style
    svn path=/Products.Poi/branches/maurits-collective.watcherlist/; revision=108011

--- Products/Poi/browser/watchers.py
+++ Products/Poi/browser/watchers.py
@@ -1,3 +1,4 @@
+from collective.watcherlist.interfaces import IWatcherList
 from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 
@@ -6,5 +7,9 @@ class WatcherView(BrowserView):
 
     def __call__(self):
         context = aq_inner(self.context)
+        # Old style
         context.toggleWatching()
+        # New style
+        watchers = IWatcherList(context)
+        watchers.toggle_watching()
         self.request.RESPONSE.redirect(context.absolute_url())
