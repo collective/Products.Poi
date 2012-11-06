654d5b1c89811b7b001de4cb9b1eec0ce675c4dd
Maurits van Rees <maurits@vanrees.org>
2012-11-06 12:02:04 +0100

    Made compatible with Plone 4.3.  Lost compatibility with Plone 4.0.

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(name='Products.Poi',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Framework :: Zope2",
         "Framework :: Zope3",
         "Programming Language :: Python",
