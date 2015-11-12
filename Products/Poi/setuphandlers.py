ea0191c6174998cd1da9537b0f8632a59dd1d513
Michael Bartz <michaelbartz@Michaels-MacBook-Pro.local>
2015-11-12 17:32:05 -0500

    remove PSC references and files

--- Products/Poi/setuphandlers.py
+++ Products/Poi/setuphandlers.py
@@ -21,26 +21,6 @@ def add_catalog_indexes(site, logger):
         catalog.manage_reindexIndex(ids=missing)
 
 
-def allow_psc_tracker(site, logger):
-    """Add PoiPscTracker to allowed types in PSCProject.
-
-    Fails gracefully if PSCProject does not exist.
-    """
-    types_tool = getToolByName(site, 'portal_types')
-    fti = getattr(types_tool, 'PSCProject', None)
-    if fti is None:
-        logger.info("Could not add PoiPscTracker to allowed content types of "
-                    "PSCProject as it does not exist.")
-        return
-    allowed_types = list(fti.allowed_content_types)
-    if 'PoiPscTracker' in allowed_types:
-        logger.info("PoiPscTracker is already in allowed content types of "
-                    "PSCProject.")
-    allowed_types.append('PoiPscTracker')
-    fti.allowed_content_types = tuple(allowed_types)
-    logger.info("Added PoiPscTracker to allowed content types PSCProject.")
-
-
 def import_various(context):
     # Only run step if a flag file is present
     if context.readDataFile('poi_various.txt') is None:
