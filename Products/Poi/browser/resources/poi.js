a830d471266c57c24ecd51b258d1bb5f1cd9e465
Chrissy Wainwright <chrissy@sixfeetup.com>
2015-11-12 15:40:41 -0500

    move css and images from skins to browser resources

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -1,15 +1,19 @@
-jQuery(function($) {
-    // No overlays for IE6
-    if (!jQuery.browser.msie ||
-        parseInt(jQuery.browser.version, 10) >= 7) {
+// jQuery(function($) {
+//     // No overlays for IE6
+//     if (!jQuery.browser.msie ||
+//         parseInt(jQuery.browser.version, 10) >= 7) {
+// 
+//         // Set up overlays
+//         $('#poi-login-form').prepOverlay({
+//             subtype: 'ajax',
+//             filter: '#content>*',
+//             formselector: '#content-core > form',
+//             noform: 'reload',
+//             closeselector: '[name=form.buttons.cancel]',
+//         });
+//     }
+// });
 
-        // Set up overlays
-        $('#poi-login-form').prepOverlay({
-            subtype: 'ajax',
-            filter: '#content>*',
-            formselector: '#content-core > form',
-            noform: 'reload',
-            closeselector: '[name=form.buttons.cancel]',
-        });
-    }
-});
+(function($) { $(function() {
+    console.log('It works!');
+}); })(jQuery);
