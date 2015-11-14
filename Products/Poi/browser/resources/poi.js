62581f80255fe3f68537d7c32169d2751d9159c2
Chrissy Wainwright <chrissy@sixfeetup.com>
2015-11-14 09:29:02 -0500

    Fix JS to not error when trying to use prepOverlay in Plone 4. Add necessary classes for the login overlay to work in Plone 5

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -1,19 +1,14 @@
-// jQuery(function($) {
-//     // No overlays for IE6
-//     if (!jQuery.browser.msie ||
-//         parseInt(jQuery.browser.version, 10) >= 7) {
-// 
-//         // Set up overlays
-//         $('#poi-login-form').prepOverlay({
-//             subtype: 'ajax',
-//             filter: '#content>*',
-//             formselector: '#content-core > form',
-//             noform: 'reload',
-//             closeselector: '[name=form.buttons.cancel]',
-//         });
-//     }
-// });
-
 (function($) { $(function() {
-    console.log('It works!');
+    // Set up overlays if Plone < 5
+    try {
+        $('#poi-login-form').prepOverlay({
+            subtype: 'ajax',
+            filter: '#content>*',
+            formselector: '#content-core > form',
+            noform: 'reload',
+            closeselector: '[name=form.buttons.cancel]',
+        });
+    } catch (e) {
+        // do nothing, the 'pat-plone-modal' class will display the overlay
+    }
 }); })(jQuery);
