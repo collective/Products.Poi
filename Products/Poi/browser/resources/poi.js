bf1cf279be8fd304d9438c276b84335df8ee6c3c
Anthony Bosio <anthony@sixfeetup.com>
2016-08-25 13:53:36 -0400

    added Upload pattern to Issue edit for attachments as child Files

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -11,4 +11,15 @@
     } catch (e) {
         // do nothing, the 'pat-plone-modal' class will display the overlay
     }
+
+    // Add file upload to Issue edit view
+    if ($('.template-edit.portaltype-issue')) {
+        $.get( "@@poi_issue_uploads", function( data ) {
+            $('.template-edit.portaltype-issue').find('#formfield-form-widgets-steps').after(data);
+            require(['pat-registry'], function (registry) {
+                registry.scan($('#formfield-form-widgets-attachments'));
+            });
+        });
+    }
+
 }); })(jQuery);
