52be4589b2e887250528e2d780c0e02c85ec46e3
Chrissy Wainwright <chrissy@sixfeetup.com>
2016-09-16 11:21:46 -0500

    add JS for loading TinyMCE for anonymous users

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -21,5 +21,20 @@
             });
         });
     }
+    
+    // require(['mockup-patterns-tinymce']);
+    require(['tinymce'], function(){
+        tinymce.init({
+          selector: '.pat-tinymce',
+          height: 500,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste code'
+          ],
+          toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+        });
+    });
+
 
 }); })(jQuery);
