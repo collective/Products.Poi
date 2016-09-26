bd06eed829f45abe17345b37f219aef3833490f2
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2016-09-26 17:20:05 -0400

    fix the elevation of priviledges to get vervision history, added plone.api to simplify the code

--- Products/Poi/browser/exporter.py
+++ Products/Poi/browser/exporter.py
@@ -6,70 +6,7 @@ from Acquisition import aq_inner
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 
-
-from AccessControl import getSecurityManager
-from AccessControl.SecurityManagement import newSecurityManager
-from AccessControl.SecurityManagement import setSecurityManager
-from AccessControl.User import UnrestrictedUser as BaseUnrestrictedUser
-
-
-class UnrestrictedUser(BaseUnrestrictedUser):
-    """Unrestricted user that still has an id.
-    """
-    def getId(self):
-        """Return the ID of the user.
-        """
-        return self.getUserName()
-
-
-def execute_under_special_role(portal, role, function, *args, **kwargs):
-    """ Execute code under special role privileges.
-
-    Example how to call::
-
-        execute_under_special_role(portal, "Manager",
-            doSomeNormallyNotAllowedStuff,
-            source_folder, target_folder)
-
-
-    @param portal: Reference to ISiteRoot object whose access controls we are
-                    using
-
-    @param function: Method to be called with special privileges
-
-    @param role: User role for the security context when calling the
-                 privileged code; e.g. "Manager".
-
-    @param args: Passed to the function
-
-    @param kwargs: Passed to the function
-    """
-
-    sm = getSecurityManager()
-
-    try:
-        try:
-            # Clone the current user and assign a new role.
-            # Note that the username (getId()) is left in exception
-            # tracebacks in the error_log,
-            # so it is an important thing to store.
-            tmp_user = UnrestrictedUser(
-                sm.getUser().getId(), '', [role], ''
-                )
-
-            # Wrap the user in the acquisition context of the portal
-            tmp_user = tmp_user.__of__(portal.acl_users)
-            newSecurityManager(None, tmp_user)
-
-            # Call the function
-            return function(*args, **kwargs)
-
-        except:
-            # If special exception handlers are needed, run them here
-            raise
-    finally:
-        # Restore the old security manager
-        setSecurityManager(sm)
+from plone import api
 
 
 class CSVExport(BrowserView):
@@ -110,14 +47,11 @@ class CSVExport(BrowserView):
         mt = getToolByName(self.context, 'portal_membership')
         for issue in issues:
             obj = issue.getObject()
-            review_history = execute_under_special_role(
-                context,
-                'Manager',
-                workflow.getInfoFor,
-                obj,
-                'review_history')
             version_history = []
-            history = rt.getHistoryMetadata(obj)
+            history = None
+            with api.env.adopt_roles(['Editor', 'Member']):
+                review_history = workflow.getInfoFor(obj, 'review_history')
+                history = rt.getHistoryMetadata(obj)
             if history:
                 retrieve = history.retrieve
                 for i in xrange(
@@ -135,16 +69,22 @@ class CSVExport(BrowserView):
             actor_info = mt.getMemberInfo(last_actor)
             if actor_info and actor_info.get("fullname", None):
                 last_actor = actor_info["fullname"]
+            else:
+                last_actor = ""
 
             row = []
             row.append(issue.id)
             row.append(issue.Title)
-            row.append(issue.target_release and issue.target_release.encode('utf-8') or "")
+            row.append(issue.target_release and
+                       issue.target_release.encode('utf-8') or "")
             row.append(obj.display_area().encode('utf-8'))
             row.append(obj.display_issue_type().encode('utf-8'))
             row.append(issue.severity.encode('utf-8'))
-            row.append(issue.assignee and pas_member.info(issue.assignee)['fullname'].encode('utf-8'))
-            row.append(", ".join(sorted((y for y in issue.Subject), key=lambda x: x.lower())))
+            row.append(issue.assignee and
+                       pas_member.info(
+                           issue.assignee)['fullname'].encode('utf-8'))
+            row.append(", ".join(sorted((y for y in issue.Subject),
+                                        key=lambda x: x.lower())))
             row.append(obj.getReviewState()['title'].encode('utf-8'))
             row.append(last_actor.encode('utf-8'))
             row.append(dateutil.parser.parse(
