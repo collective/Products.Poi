8438923fb44602cd3b8e05045cc83a8e88764267
Chrissy Wainwright <chrissy@sixfeetup.com>
2016-10-12 14:57:54 -0500

    Explicity enable the visual editor on two fields
    Adjusted how the visual editor is loaded for anonymous users
    by explicitly settings the fields. This fixes the issue of the
    fields not saving the content.

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -24,34 +24,29 @@
         });
     }
 
-    // require(['mockup-patterns-tinymce']);
-
     // manually load tinymce is user is not authenticated...
     if($('body.userrole-anonymous.template-products-poi-content-issue-issue').length === 1){
-      require(['tinymce'], function(){
-        $('.pat-textareamimetypeselector').each(function(){
-          // when using the textareamimetypeselector pattern, the textarea
-          // is the previous element
-          var $tinymce = $(this).prev();
-          // hide the textareamimetypeselector
-          $(this).hide();
-
-          // we need an id for the tinymce instance
-          var id = 1 + Math.floor(100000 * Math.random());
-          $tinymce.attr('id', id);
-
-          tinymce.init({
-            selector: "#" + id,
-            height: 500,
-            plugins: [
-              'advlist autolink lists link image charmap print preview anchor',
-              'searchreplace visualblocks code fullscreen',
-              'insertdatetime media table contextmenu paste code'
-            ],
-            toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
-          });
+        require(['tinymce'], function(){
+          displayEditor($("#formfield-form-widgets-details").find('textarea'));
+          displayEditor($("#formfield-form-widgets-steps").find('textarea'));
         });
-      });
     }
 
 }); })(jQuery);
+
+function displayEditor(field) {
+    // we need an id for the tinymce instance
+    var id = 1 + Math.floor(100000 * Math.random());
+    $(field).attr('id', id);
+
+    tinymce.init({
+      selector: "#" + id,
+      height: 500,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code'
+      ],
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+    });
+}
