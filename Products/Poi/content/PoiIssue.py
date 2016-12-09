058379aa2866625e8b4a038bb4b1c230c5e8c0ae
Maurits van Rees <maurits@vanrees.org>
2016-12-09 20:06:45 +0100

    Only do link detection in plain or intelligent text fields.
    If a field has html and contains an html space `&#160` and you have an
    issue 160, it will start looking very ugly.

--- Products/Poi/content/PoiIssue.py
+++ Products/Poi/content/PoiIssue.py
@@ -74,6 +74,11 @@ from Products.Poi.interfaces import ITracker
 from Products.Poi import PoiMessageFactory as _
 from plone.memoize import instance
 
+PLAIN_MIMETYPES = (
+    'text/x-web-intelligent',
+    'text/plain',
+)
+
 schema = Schema((
 
     StringField(
@@ -663,19 +668,23 @@ ${issue_details}
     def setSteps(self, *args, **kwargs):
         self.getField('steps').set(self, *args, **kwargs)
 
-    @instance.memoize
-    def getTaggedDetails(self, **kwargs):
+    def _link_detect_text(self, fieldname, **kwargs):
         # perform link detection
-        text = self.getField('details').get(self, **kwargs)
+        field = self.getField(fieldname)
+        text = field.get(self, **kwargs)
+        if field.getContentType(self) not in PLAIN_MIMETYPES:
+            # Don't touch text/html or similar.
+            return text
         tracker = self.getTracker()
         return tracker.linkDetection(text)
 
     @instance.memoize
+    def getTaggedDetails(self, **kwargs):
+        return self._link_detect_text('details', **kwargs)
+
+    @instance.memoize
     def getTaggedSteps(self, **kwargs):
-        # perform link detection
-        text = self.getField('steps').get(self, **kwargs)
-        tracker = self.getTracker()
-        return tracker.linkDetection(text)
+        return self._link_detect_text('steps', **kwargs)
 
     def getTracker(self):
         """Return the tracker.
