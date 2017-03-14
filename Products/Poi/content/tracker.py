7eb06a2c9162cf3da95a1fa354eba43801b7a554
Anthony Bosio <anthony@sixfeetup.com>
2017-03-14 10:08:33 -0400

    add dynamic local role for assignees to become Technicians.

--- Products/Poi/content/tracker.py
+++ Products/Poi/content/tracker.py
@@ -24,8 +24,10 @@ __author__ = """Rob McBroom <rob@sixfeetup.com>"""
 __docformat__ = 'plaintext'
 
 
+from zope.component import adapts
 from zope.interface import implementer
 from zope import schema
+from zope.interface import implements
 from zope.interface import Interface
 from zope.interface import directlyProvides
 from zope.interface import provider
@@ -33,6 +35,8 @@ from zope.schema.interfaces import IContextAwareDefaultFactory
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
+
+from borg.localrole.interfaces import ILocalRoleProvider
 
 from collections import Counter
 from Products.Poi import PoiMessageFactory as _
@@ -436,3 +440,28 @@ class Tracker(Container):
         if self.repo_url:
             text = link_repo(text, self.repo_url)
         return text
+
+
+class TrackerRoleProvider(object):
+    """ role provider to give all Assignees the Technician role
+    """
+    implements(ILocalRoleProvider)
+    adapts(ITracker)
+
+    def __init__(self, context):
+        self.context = context
+        self.roles = (u'Technician',)
+
+    def getRoles(self, user_id):
+        try:
+            if user_id not in self.context.assignees:
+                return ()
+        except AttributeError:
+            return ()
+        return self.roles
+
+    def getAllRoles(self):
+        try:
+            yield [(i, self.roles) for i in self.context.assignees]
+        except AttributeError:
+            yield '', ()
