3a08b77efc93770d8c202bdddad6fbb77bd81220
Anthony Bosio <anthony@sixfeetup.com>
2017-03-27 16:02:35 -0400

    avoid tooltip name conflict between Bootstrap and jQueryUI.

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -47,6 +47,7 @@
     // add tooltips for automatic linking
     if ($('.portaltype-issue').length>0) {
         $("label[for='form-widgets-details'] .formHelp, #response_help").append(" This field can create Automatic Links.<a href='@@poi_issue_automatic_linking' class='automatic-linking' title='test'><span class='icon-contentInfo'></span></a>");
+        var bootstrapTooltip = $.fn.tooltip.noConflict();
         $(document).tooltip({
             items: '.automatic-linking',
             content: function(callback){
