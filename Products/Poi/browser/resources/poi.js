31ba118703a892905aa30b22fbc5eabd3cfeb162
Chrissy Wainwright <chrissy@sixfeetup.com>
2017-03-29 14:16:42 -0500

    remove image button from anonymous TinyMCE fields

--- Products/Poi/browser/resources/poi.js
+++ Products/Poi/browser/resources/poi.js
@@ -89,10 +89,10 @@ function displayEditor(field) {
       selector: "#" + id,
       height: 500,
       plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
+        'advlist autolink lists link charmap print preview anchor',
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table contextmenu paste code'
       ],
-      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link'
     });
 }
