bb66a6b085f7237ccf32dcd11e5d511a5da7effb
Guido A.J. Stevens <guido.stevens@cosent.nl>
2018-02-26 10:23:15 +0000

    Try to reproduce i18ndude bug as reported in https://github.com/collective/i18ndude/pull/48 but fails:
    There was an error processing
    Products/Poi/skins/Poi/poi-issue-search-rss.xml.pt
    Traceback (most recent call last):
      File "/var/tmp/eggs/i18ndude-4.0.1-py2.7.egg/i18ndude/extract.py",
    line 523, in tal_strings
        parser.parseFile(filename)
          File
    "/var/tmp/eggs/zope.tal-3.5.2-py2.7.egg/zope/tal/htmltalparser.py",
    line 122, in parseFile
        self.parseString(data)
          File
    "/var/tmp/eggs/zope.tal-3.5.2-py2.7.egg/zope/tal/htmltalparser.py",
    line 128, in parseString
        self.feed(data)
          File "/usr/lib/python2.7/HTMLParser.py", line 117, in feed
              self.goahead(0)
    	    File "/usr/lib/python2.7/HTMLParser.py", line 161, in goahead
    	        k = self.parse_starttag(i)
    		  File "/usr/lib/python2.7/HTMLParser.py", line 325, in parse_starttag
    		      self.handle_startendtag(tag, attrs)
    		        File
    "/var/tmp/eggs/zope.tal-3.5.2-py2.7.egg/zope/tal/htmltalparser.py",
    line 169, in handle_startendtag
        self.getpos())
        TALError: empty HTML tags cannot use tal:content: 'link', at line
    13, column 5, in file Products/Poi/skins/Poi/poi-issue-search-rss.xml.pt

---
+++ rebuild_i18n.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+I18NDOMAIN="Poi"
+BASE_DIRECTORY="Products/Poi"
+
+# Synchronise the templates and scripts with the .pot.
+bin/i18ndude rebuild-pot --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot \
+    --create ${I18NDOMAIN} \
+    ${BASE_DIRECTORY}
+
+# Synchronise the resulting .pot with all .po files
+for po in ${BASE_DIRECTORY}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    bin/i18ndude sync --pot ${BASE_DIRECTORY}/locales/${I18NDOMAIN}.pot $po
+done
+
+# Report of errors and suspect untranslated messages
+bin/i18ndude find-untranslated -n ${BASE_DIRECTORY}
