575294fdb9cb6838abd1ddbeba34d722f7767402
Anthony Bosio <anthony@sixfeetup.com>
2020-01-09 11:41:50 -0500

    update setup.py info

--- setup.py
+++ setup.py
@@ -11,18 +11,18 @@ historyfile.close()
 long_description = readme + "\n\n" + history
 
 setup(name='Products.Poi',
-      version='4.0.3.dev0',
+      version='4.1.dev0',
       description="Poi: A friendly issue tracker",
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.2",
           "Framework :: Zope2",
           "Framework :: Zope3",
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
           ],
       keywords='bugs issue tracker',
